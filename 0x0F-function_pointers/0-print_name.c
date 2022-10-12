85337bd3270f2ade523ac41f6d4667b25652fc82
Your Name <you@example.com>
2022-10-12 16:16:25 -0700

    debugged 0-print

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -8,7 +9,8 @@
 **/
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+	if (!name || !f)
+		return;
 
+	f(name);
 }
